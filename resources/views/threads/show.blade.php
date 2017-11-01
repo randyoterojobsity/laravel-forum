ec45b2479e0d855bad63e827472518ffb2046791
Randy Otero <randy.otero@jobsity.com>
2017-11-01 12:28:27 -0500

    Lesson 16

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h2>{{ $thread->title }}</h2>
@@ -11,22 +11,15 @@
                     </div>
                     <div class="panel-body">{{ $thread->body }}</div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+                <hr>
                 <h3>List of Replies</h3>
-                @foreach($thread->replies as $reply)
+
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
+                {{ $replies->links() }}
 
-        <hr>
-
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
                 @if(auth()->check())
                     <h4>Create a reply</h4>
                     <form method="POST" action="{{ $thread->path().'/replies' }}">
@@ -43,7 +36,19 @@
                     <p>Please <a href="{{ route('login') }}">log in</a> to participate in this discussion.</p>
                 @endif
             </div>
-        </div>
 
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This Thread was published {{ $thread->created_at->diffForHumans() }} <br>
+                            by <a href="{{ $thread->creator->path() }}">{{ $thread->creator->name }}</a> <br>
+                            and curretly
+                            has {{ $thread->replies_count }} {{ str_plural('comment',$thread->replies_count) }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
