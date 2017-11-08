3a5146376a94ff386d6f9f48261b070483a74fd3
Randy Otero <randy.otero@jobsity.com>
2017-11-08 11:58:50 -0500

    Lesson 21

--- app/Reply.php
+++ app/Reply.php
@@ -8,6 +8,8 @@ class Reply extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['owner', 'favorites'];
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
@@ -34,7 +36,12 @@ class Reply extends Model
 
     public function isFavorited()
     {
-        return $this->favorites()->where('user_id', auth()->id())->exists();
+        return ! ! $this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
     }
 }
 
